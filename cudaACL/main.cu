88842f6bd5ae1f488eca0dbfb0092531a39e8ab9
Dakota Williams <dwms9395@verizon.net>
2016-06-12 15:36:10 -0400

    cudaACL: initial revision of kernel func

--- cudaACL/main.cu
+++ cudaACL/main.cu
@@ -8,7 +8,7 @@
 #include <netinet/ip.h>
 
 enum protocolType {
-	IP,
+	IP, //both
 	TCP,
 	UDP
 };
@@ -23,15 +23,54 @@ struct aclRule {
 	uint16_t port;
 };
 
+
+int eval(struct in_addr srcIP, struct in_addr dstIP, enum protocolType proto, uint16_t port, struct aclRule* rules, int nRules) { //bool
+	for(int i = 0; i < nRules; i++) {
+		//if it doesn't apply ignore
+		if((srcIP & rules[i].src_mask) != rules[i].src_network || (dstIP & rules[i].dst_mask) != rules[i].dst_network
+				|| proto == IP || proto != rules[i].proto || port != rules[i].port) {
+			continue;
+		}
+		
+		//the rule applies, allow or deny
+		return rules[i].allow;
+	}
+
+	//implicit deny all
+	return 0;
+}
+
+//<<<1, n, 2*n>>>
 __global__
-int kernel(struct in_addr srcIP, struct in_addr dstIP, enum protocolType proto, uint16_t port, struct aclRule* rules, int nRules) { //bool
+void eval_kernel(struct in_addr srcIP, struct in_addr dstIP, enum protocolType proto, uint16_t port, struct aclRule* rules, int nRules, uint8_t* out) { //bool
+	extern __shared__ uint8_t buf[];
+	uint8_t* apply = &buf[0];
+	uint8_t* pass = &buf[nRules];
+
 	int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
 	if(tid > nRules) {
 		return 1; //true
 	}
 
+	apply[tid] = ((srcIP & rules[tid].src_mask) != rules[tid].src_network || (dstIP & rules[tid].dst_mask) != rules[tid].dst_network
+			|| proto == IP || proto != rules[tid].proto || port != rules[tid].port);
+	pass[tid] = rules[tid];
+	
+	syncThreads();
 
 	//reduce 
+	for(unsigned int i = blockDim.x/2; i > 0; i >>= 1) {
+		if(apply[tid+i]) {
+			pass[tid] &= pass[tid+1];
+		} else {
+			pass[tid+i] = 1;
+		}
+
+		syncThreads();
+	}
+
+	&out = pass[0];
 }
 
 int main(int argc, char** argv) {
